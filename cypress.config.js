d1f48b72e53b505d0b10a146b67d98a64bf219b1
wareval0 <w.arevalo@uniandes.edu.co>
2026-01-28 09:45:26 -0500

    Cypress Debugging and Debriefing

--- cypress.config.js
+++ cypress.config.js
@@ -2,15 +2,28 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    // URL base de nuestra aplicación (Swag Labs)
     baseUrl: 'https://www.saucedemo.com',
-    // Patrón para encontrar nuestros archivos de prueba
     specPattern: 'cypress/e2e/**/*.cy.js',
-    // Deshabilitamos video por defecto (lo activaremos luego)
-    video: false,
     chromeWebSecurity: false,
+
+    // --- Configuración de Debugging y Estabilidad ---
+
+    // 1. Habilitamos video para ver la repetición de la jugada
+    video: true,
+    // Comprimimos el video para que no ocupe mucho espacio (0 a 51)
+    videoCompression: 32,
+
+    // 2. Screenshots: Por defecto es true al fallar, pero lo explicitamos
+    screenshotOnRunFailure: true,
+
+    // 3. Retries: Reintentar pruebas fallidas para evitar 'Flakiness'
+    retries: {
+      runMode: 1, // En CLI (Headless): reintenta 1 vez extra
+      openMode: 0 // En GUI (Local): no reintentar, quiero ver el error ya
+    },
+
     setupNodeEvents(on, config) {
-      // listeners
+      // Listeners de eventos
     },
   },
 });
