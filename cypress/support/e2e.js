e15a96819bae5de241f1781cf1e9ef90049031b9
wareval0 <w.arevalo@uniandes.edu.co>
2026-01-28 10:48:05 -0500

    Cypress headless Codelab uploaded

--- cypress/support/e2e.js
+++
@@ -1,7 +0,0 @@
-// Importamos los comandos por defecto
-import './commands';
-
-// Registramos el reporter para que escuche los fallos
-import 'cypress-mochawesome-reporter/register';
-
-// (Opcional) Podemos silenciar logs XHR ruidosos aquí si quisiéramos
